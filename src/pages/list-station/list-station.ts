6660d0f8e2c44db19f7b71fa7f31fa3f96eb67a4
minh.nguyen <minhnb89@gmail.com>
2017-06-29 11:58:41 +0700

    TRV-91 [Staff, Driver] #2. Driver can see a list of assigned stations

--- src/pages/list-station/list-station.ts
+++ src/pages/list-station/list-station.ts
@@ -2,47 +2,74 @@ import { Component, Injector } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BaseComponent } from '../../app/base.component';
 import { DirectionStopPage } from '../direction-stop';
+import { CollectionModeService } from '../../app/services/collection-mode';
 
 @IonicPage()
 @Component({
 	selector: 'page-list-station',
 	templateUrl: 'list-station.html',
+	providers: [CollectionModeService]
 })
 export class ListStationPage extends BaseComponent {
 
     listStation: Array<any> = [];
+	isSelectNextStationMode: boolean = false;
 
-	constructor(private injector: Injector, public navCtrl: NavController, public navParams: NavParams) {
+	constructor(private injector: Injector, public navCtrl: NavController, public navParams: NavParams, private collectionModeService: CollectionModeService) {
 		super(injector);
-        this.listStation = [
-            {
-                name: "Crescent Mall"
-            },
-            {
-                name: "Lotte Mart"
-            },
-            {
-                name: "Vincom Mall"
-            },
-            {
-                name: "Nowzone Mall"
-            },
-            {
-                name: "TSN Airport"
-            }
-        ]
+		if (navParams.data.isSelectNextStationMode) {
+			this.isSelectNextStationMode = true;
+		}
+
+		if (this.isSelectNextStationMode) {
+			let currentStation = navParams.data.currentStation;
+			this.removeCurrentStationInList(currentStation);
+		}
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad ListStationPage');
+		this.loadListStation();
+	}
+
+	removeCurrentStationInList(currentStation: any) {
+		let index = this.indexOfStationInList(currentStation);
+		this.listStation.splice(index, 1);
+	}
+
+	indexOfStationInList(station: any) {
+		for (let i = 0; i < this.listStation.length; i++) {
+			if (station.name == this.listStation[i].name) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	loadListStation() {
+		this.collectionModeService.getListStation().subscribe(
+			res => {
+				this.listStation = res;
+			},
+			err => {
+				this.showError(err.message);
+			}
+		);
 	}
 
     viewDirection(station: any) {
         this.navCtrl.push(DirectionStopPage, {
-            name: station.name,
-            long: 106.702013,
-            lat: 10.740790
+            station: station,
+            long: station.lng,
+            lat: station.lat
         });
     }
+
+	chooseNextStation(station: any) {
+		let params = {
+			nextStation: station
+		}
+		this.navCtrl.pop(params);
+	}
 
 }
