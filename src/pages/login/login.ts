cdea1e68e33ab29495178d999e1237d4f3a89903
minh.nguyen <minhnb89@gmail.com>
2017-07-19 15:04:07 +0700

    Init user state when login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,10 @@
 import { Component, Injector } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { BaseComponent } from '../../app/base.component';
+import { AppConstant } from '../../app/app.constant';
+
 import { UserService } from '../../app/services/user';
+
 import { UserStartPage } from '../user-start';
 
 @Component({
@@ -16,7 +19,7 @@ export class LoginPage extends BaseComponent {
 
 	constructor(private injector: Injector, public navCtrl: NavController, public navParams: NavParams, private userService: UserService) {
 		super(injector);
-		this.username = 'test@attendant.zappp.com';
+		this.username = 'test@driver.zappp.com';
 		this.password = '1234567';
 	}
 
@@ -28,6 +31,7 @@ export class LoginPage extends BaseComponent {
 		this.userService.userLogIn(this.username, this.password).subscribe(
 			res => {
 				if (this.isDriver() || this.isAttedant() || this.isZappper()) {
+					this.saveLocalState(res);
 					this.navCtrl.setRoot(UserStartPage);
 				} else {
 					this.showError(this.translate.instant('USER_NOT_STAFF'));
@@ -38,5 +42,19 @@ export class LoginPage extends BaseComponent {
 				this.showError(err.message);
 			}
 		)
+	}
+
+	saveLocalState(res: any) {
+		if (!res.user) {
+			return;
+		}
+		let user = res.user;
+		if (user.is_availability == '1') {
+			let status = true;
+			localStorage.setItem(AppConstant.STATUS, status.toString());
+			if ((this.isDriver() || this.isAttedant()) && user.truck_info) {
+				localStorage.setItem(AppConstant.TRUCK, user.truck_info.id);
+			}
+		}
 	}
 }
