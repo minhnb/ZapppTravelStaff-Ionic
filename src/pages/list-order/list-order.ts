67315429e21d7bc75801dcefee1c81df29354330
minh.nguyen <minhnb89@gmail.com>
2017-07-20 16:35:40 +0700

    TRV-179, TRV-180 TRV-179 [Staff, ATTENDANT][Delivery] #4. Delivery. Attendant can select a hotel and view a list of relevant orders
    TRV-180
    [Staff, Attendant][Delivery] #5. Attendant can select an order (a sender) to view order details

--- src/pages/list-order/list-order.ts
+++ src/pages/list-order/list-order.ts
@@ -1,13 +1,17 @@
 import { Component, Injector } from '@angular/core';
 import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { BaseComponent } from '../../app/base.component';
+
 import { CustomerLuggagePage } from '../customer-luggage';
 import { DeliveryInfoPage } from '../delivery-info';
+
+import { CollectionModeService } from '../../app/services/collection-mode';
 
 @IonicPage()
 @Component({
 	selector: 'page-list-order',
 	templateUrl: 'list-order.html',
+	providers: [CollectionModeService]
 })
 export class ListOrderPage extends BaseComponent {
 
@@ -16,7 +20,8 @@ export class ListOrderPage extends BaseComponent {
 	isDeliveryMode: boolean = false;
 	deliveryItem: any;
 
-	constructor(private injector: Injector, public navCtrl: NavController, public navParams: NavParams, public events: Events) {
+	constructor(private injector: Injector, public navCtrl: NavController, public navParams: NavParams, public events: Events,
+		private collectionModeService: CollectionModeService) {
 		super(injector);
 		this.pageName = navParams.data.pageName;
 		this.listOrder = navParams.data.listOrder;
@@ -46,10 +51,18 @@ export class ListOrderPage extends BaseComponent {
 	}
 
 	goToDeliveryInfo(customer: any) {
-		let params = {
-            customer: customer
-        }
-		this.navCtrl.push(DeliveryInfoPage, params);
+		this.collectionModeService.getOrderDetail(customer.orderId).subscribe(
+			res => {
+				let customerInfo = this.customerInfoTransform(res);
+				let params = {
+					customer: customerInfo
+				}
+				this.navCtrl.push(DeliveryInfoPage, params);
+			},
+			err => {
+				this.showError(err.message);
+			}
+		);
 	}
 
 	removeDeliveryItem() {
