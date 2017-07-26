a87d61e2f28ad681ef41acf9da29de081ffc1ec2
minh.nguyen <minhnb89@gmail.com>
2017-07-26 14:21:20 +0700

    TRV-304 [Staff, Driver, Attendant] Choose a truck, truck list showed wrong, missed some available trucks
    Don't allow staff to change truck when they are active. They have to release current truck (by set status to inactive) and set status active again to choose new truck.

--- src/pages/user-start/user-start.ts
+++ src/pages/user-start/user-start.ts
@@ -105,10 +105,19 @@ export class UserStartPage extends BaseComponent {
 		this.events.publish('user:active', params);
 	}
 
+	resetInfoForDriverAndAttendant() {
+		this.truck = null;
+		this.listAssignment = [];
+		this.countNewAssignment();
+	}
+
     onStatusChange(event) {
 		this.staffService.updateStatus(this.isActive).subscribe(
 			res => {
 				this.saveStatusToLocalStorage(this.isActive);
+				if (!this.isActive) {
+					this.resetInfoForDriverAndAttendant();
+				}
 				this.announceActiveEvent();
 				this.loadListTruckForActiveDirverAndAttendant();
 				this.loadJobForActiveZappper();
@@ -139,6 +148,7 @@ export class UserStartPage extends BaseComponent {
 		this.staffService.chooseTruck(truckId).subscribe(
 			res => {
 				this.saveTruckInfoToLocalStorage(truckId);
+				this.loadListAssignment();
 			},
 			err => {
 				this.showError(err.message);
@@ -329,6 +339,9 @@ export class UserStartPage extends BaseComponent {
 	}
 
 	loadListAssignment(callback?: () => void) {
+		if (this.isZappper() || !this.isActive) {
+			return;
+		}
 		this.staffService.loadListAssignment(false).subscribe(
 			res => {
 				this.listAssignment = res;
