45f3bd70a267a7354e42e3048b95ddf6a3578e36
minh.nguyen <minhnb89@gmail.com>
2017-08-25 11:14:09 +0700

    Update production server url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
 export const AppConfig = {
-    // API_URL: 'https://dev.api.zappp.com/api/',
-    API_URL: 'https://stage.api.zappp.com/api/',
+    API_URL: 'https://api.zappp.com/api/',
     GOOGLE_API_KEY: 'AIzaSyCdHJWVxOf18WOxZmsttuF3MI_Emy9EQH4'
 };
