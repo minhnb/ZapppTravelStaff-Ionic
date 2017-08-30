918d89d091118c3dcb7d5bb5809cad81cad9995d
minh.nguyen <minhnb89@gmail.com>
2017-08-30 17:50:03 +0700

    Update production environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
     API_URL: 'https://api.zappp.com/api/',
     GOOGLE_API_KEY: 'AIzaSyCyThSMQW1kGzznD-Wa1IU8m8biWwCojZA',
-    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105191661-1',
-    ENV: 'dev'
+    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105632473-1',
+    ENV: 'production'
 };
