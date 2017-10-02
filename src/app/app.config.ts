c102b284098b4ed6188b2df353c6dd74d430f3d0
minh.nguyen <minhnb89@gmail.com>
2017-10-02 17:36:54 +0700

    Merge branch 'master' into staging (reverted from commit 9fca912936669ad81c3cb1f546db9c2259bc081a)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 export const AppConfig = {
-    API_URL: 'https://api.zappp.com/api/',
-    SOCKET_IO_URL: 'http://chat.api.zappp.com/',
-    GOOGLE_API_KEY: 'AIzaSyCyThSMQW1kGzznD-Wa1IU8m8biWwCojZA',
-    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105632473-1',
-    ENV: 'production'
+    API_URL: 'https://stage.api.zappp.com/api/',
+    // SOCKET_IO_URL: 'http://dev.chat.api.zappp.com/',
+    SOCKET_IO_URL: 'http://stage.chat.api.zappp.com/',
+    GOOGLE_API_KEY: 'AIzaSyCdHJWVxOf18WOxZmsttuF3MI_Emy9EQH4',
+    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105191661-1',
+    ENV: 'dev'
 };
