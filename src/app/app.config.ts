702feaa4b06e5c152c7721c41c89432b96c0d75f
minh.nguyen <minhnb89@gmail.com>
2017-10-13 15:47:00 +0700

    Re-update app setting for server production after merge.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
     API_URL: 'https://api.zappp.com/api/',
-    SOCKET_IO_URL: 'http://chat.api.zappp.com/',
+    SOCKET_IO_URL: 'https://chat.api.zappp.com/',
     GOOGLE_API_KEY: 'AIzaSyCyThSMQW1kGzznD-Wa1IU8m8biWwCojZA',
     GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105632473-1',
     ENV: 'production'
