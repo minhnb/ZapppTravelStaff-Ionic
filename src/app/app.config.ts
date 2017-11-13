34358dad0d29bd98b33e773955430098fd142c92
minh.nguyen <minhnb89@gmail.com>
2017-11-13 14:23:09 +0700

    Update production settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,7 @@
 export const AppConfig = {
-   // API_URL: 'https://dev.api.zappp.com/api/',
-    API_URL: 'https://stage.api.zappp.com/api/',
-    // SOCKET_IO_URL: 'https://dev.chat.api.zappp.com/',
-    SOCKET_IO_URL: 'https://stage.chat.api.zappp.com/',
-    GOOGLE_API_KEY: 'AIzaSyCdHJWVxOf18WOxZmsttuF3MI_Emy9EQH4',
-    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105191661-1',
-    ENV: 'dev'
+    API_URL: 'https://api.zappp.com/api/',
+	SOCKET_IO_URL: 'http://chat.api.zappp.com/',
+	GOOGLE_API_KEY: 'AIzaSyCyThSMQW1kGzznD-Wa1IU8m8biWwCojZA',
+	GOOGLE_ANALYTICS_TRACKING_ID: 'UA-105632473-1',
+	ENV: 'production'
 };
